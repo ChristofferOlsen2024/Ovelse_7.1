6f61248f2eceecb57d261a429254ba8ae1a62ec3
ChristofferOlsen2024 <ch.o@mailfence.com>
2024-06-12 09:54:35 +0200

    1.

--- Ovelse_7.1/Hilsen.cs
+++ Ovelse_7.1/Hilsen.cs
@@ -9,8 +9,8 @@ namespace Ovelse_7._1
     internal class Hilsen
     {
         public void SigHej()
-        { 
-            
+        {
+            Console.WriteLine("He Verden");
         }
     }
 }
